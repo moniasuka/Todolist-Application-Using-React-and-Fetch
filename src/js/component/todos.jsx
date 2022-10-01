d8ed642ec7a31a8db219eb77c18ce2d760aca394
moniasuka <moniasuka@hotmail.com>
2022-10-01 21:42:52 +0000

    agregando los contadores para incrementar o decrementar tareas de la lista

--- src/js/component/todos.jsx
+++ src/js/component/todos.jsx
@@ -5,7 +5,7 @@ export const Todo = () => {
     //1) estructura de datos adecuada: Arreglos
     //["tarea1" , "lavar platos" , "sacar la basura"]
     //Para agregar utilizamos el metodo .push()
-    //Poder recorrer los arreglos con For, .map((item, index)=>{return()})
+    //Poder recorrer los arreglos usamos .map((item, index)=>{return()})
     //Podemos remover .filter((item, index)=>{return(index != 2)})
 
     const [listTodos, setListTodos] = useState([])
@@ -13,37 +13,70 @@ export const Todo = () => {
 
 
 
+    //contador
+    const [count, setCount] = useState(0);
+
+   
+    
+        // Function to increment count by 1
+        const decrementCount = () => {
+        // Update state with decremented value
+        setCount(count - 1);
+        };
+
+       // Function to increment count by 1
+       const incrementCount = () => {
+        // Update state with incremented value
+        setCount(count + 1);
+        };
+
+
+
+
+    //para borrar tareas
+
     const deleteTodo = (indiceTarea) => {   
     setListTodos((prevState) => 
     {return prevState.filter((item, index) => { return index !== indiceTarea;})}
     )};
 
+   
 
 
     return (
         
         <div className="card d-inline-flex p-2 w-50">
+            
             <input placeholder="What needs to be done?" type="text" onKeyUp={(e)=>{
                 if(e.keyCode =="13"){
                     let arrAux = listTodos.slice()
                     arrAux.push(e.target.value)
                     setListTodos(arrAux)
                     e.target.value="";
+                    incrementCount();
+                    
                 }
             }}
             />
                 
             <ul>
-                {listTodos.length>0 ? listTodos.map((tarea, index) => {        
+               
+                {listTodos.length>0 ? listTodos.map((tarea, index) => {  
+                         
                     return (
                         <>
-                        <li className="list-group-item d-flex" key={index}>  {tarea}  <button onClick={(e)=>{deleteTodo(index)}}><i className="fa fa-times align-items-end"></i></button> </li> 
+                        <li className="list-group-item d-flex justify-content-between" key={index}>  {tarea}  <button className="btn btn-outline-light" onClick={(e)=>{deleteTodo(index); decrementCount()}}><i className="fa fa-times"></i></button> </li> 
+                    
                         </>
+                    
                     )
-                
+                 
                 }) :   <> </> }
-            
+           
             </ul>
+            <div>
+               <p>{count} items left</p> 
+            </div>
         </div>
         
 
